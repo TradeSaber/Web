8fa5d075d420583633592192be3b50402dcd80f0
Auros Nexus <aurosnex@gmail.com>
2021-03-10 11:56:05 -0500

    *gone*

--- src/variables/index.ts
+++
@@ -1,3 +0,0 @@
-
-export const API_URL = process.env.NODE_ENV === "development" ? "https://localhost:44307/api/asria" : "https://tradesaber.com/api/asria"
-export const BASE_URL = process.env.NODE_ENV === "development" ? "https://localhost:44307" : "https://tradesaber.com"
