d58f4c48f16fc59e95a8bc0db30ea0cc94941d5b
Auros Nexus <aurosnex@gmail.com>
2021-03-11 09:04:53 -0500

    :red_car: Basic Navbar

--- tradesaber/src/components/TradeNav.tsx
+++ tradesaber/src/components/TradeNav.tsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { Container, Navbar } from 'react-bulma-components'
+import { Button, Container, Navbar } from 'react-bulma-components'
 import smallLongLogo from '../assets/logo-long-small.png'
 
 export default class TradeNav extends Component {
@@ -12,6 +12,42 @@ export default class TradeNav extends Component {
                             <img src={smallLongLogo} alt="Trade Saber Logo"  />
                         </Navbar.Item>
                     </Navbar.Brand>
+                    <Navbar.Menu>
+                        <Navbar.Container>
+                            <Navbar.Item hoverable href="#">
+                                Home
+                            </Navbar.Item>
+                            <Navbar.Item dropdown hoverable href="#">
+                                <Navbar.Link arrowless={false}>
+                                    Collections
+                                </Navbar.Link>
+                                <Navbar.Dropdown>
+                                    <Navbar.Item href="#">
+                                        Cards
+                                    </Navbar.Item>
+                                    <Navbar.Item href="#">
+                                        Series
+                                    </Navbar.Item>
+                                    <Navbar.Item href="#">
+                                        Packs
+                                    </Navbar.Item>
+                                </Navbar.Dropdown>
+                            </Navbar.Item>
+                            <Navbar.Item href="#">
+                                Download
+                            </Navbar.Item>
+                            <Navbar.Item href="#">
+                                FAQ
+                            </Navbar.Item>
+                        </Navbar.Container>
+                        <Navbar.Container position="end">
+                            <Navbar.Item href="#">
+                                <Button>
+                                    Log In
+                                </Button>
+                            </Navbar.Item>
+                        </Navbar.Container>
+                    </Navbar.Menu>
                 </Container>
             </Navbar>
         )
