bd88d6bdf2ac4d3bb9c546eacf3fcb3a1127bf2c
Auros Nexus <aurosnex@gmail.com>
2021-05-25 11:20:27 -0400

    TradeSaberSharp Import and Setup

--- TradeSaber/Program.cs
+++ TradeSaber/Program.cs
@@ -3,10 +3,14 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Net.Http;
+using System.Text.Json;
 using System.Threading.Tasks;
 using TradeSaber.Interfaces;
 using TradeSaber.Services;
 using TradeSaber.Settings;
+using TradeSaberSharp;
+using TradeSaberSharp.Core;
+using TradeSaberSharp.Json;
 
 namespace TradeSaber
 {
@@ -14,6 +18,8 @@ namespace TradeSaber
     {
         public static async Task Main(string[] args)
         {
+            ISerializer serializer = new SystemTextJsonSerializer(new JsonSerializerOptions(JsonSerializerDefaults.Web));
+
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("#app");
 
@@ -22,6 +28,7 @@ namespace TradeSaber
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
             builder.Services.AddScoped<ILocalUserService, LocalUserService>();
             builder.Services.ConstructSetting<IURLSettings, URLSettings>(builder.Configuration);
+            builder.Services.AddScoped(sp => new TradeSaberClient(builder.Configuration["URLSettings:API"], serializer, new HttpClientHttpService(new HttpClient(), serializer)));
 
             await builder.Build().RunAsync();
         }
--- TradeSaber/Services/LocalUserService.cs
+++ TradeSaber/Services/LocalUserService.cs
@@ -1,22 +1,27 @@
 ﻿using Blazored.LocalStorage;
 using System.Threading.Tasks;
 using TradeSaber.Interfaces;
+using TradeSaberSharp;
 
 namespace TradeSaber.Services
 {
     public class LocalUserService : ILocalUserService
     {
         private const string _tokenKey = "token";
+        private readonly TradeSaberClient _tradeSaberClient;
         private readonly ILocalStorageService _localStorageService;
 
-        public LocalUserService(ILocalStorageService localStorageService)
+        public LocalUserService(TradeSaberClient tradeSaberClient, ILocalStorageService localStorageService)
         {
+            _tradeSaberClient = tradeSaberClient;
             _localStorageService = localStorageService;
         }
 
         public async Task<string?> GetToken()
         {
-            return await _localStorageService.GetItemAsync<string>(_tokenKey);
+            string? token = await _localStorageService.GetItemAsync<string>(_tokenKey);
+            _tradeSaberClient.Token = token;
+            return token;
         }
 
         public async Task SetToken(string? token)
@@ -27,6 +32,7 @@ namespace TradeSaber.Services
                 return;
             }
             await _localStorageService.SetItemAsync(_tokenKey, token);
+            _tradeSaberClient.Token = await GetToken();
         }
     }
 }
