582a71b7e6b351e21aee9d1413544f0e48bf56e2
Auros Nexus <aurosnex@gmail.com>
2021-06-15 08:47:56 -0400

    Removed debug logging

--- TradeSaber/Authorization/TradeAuthStateProvider.cs
+++ TradeSaber/Authorization/TradeAuthStateProvider.cs
@@ -1,5 +1,4 @@
 ﻿using Microsoft.AspNetCore.Components.Authorization;
-using System;
 using System.Security.Claims;
 using System.Threading.Tasks;
 using TradeSaber.Interfaces;
@@ -19,11 +18,9 @@ namespace TradeSaber.Authorization
 
         public override Task<AuthenticationState> GetAuthenticationStateAsync()
         {
-            Console.WriteLine("hello yes i am being checked");
             var identity = new ClaimsIdentity(_localUserService.ActiveUser is null ? null : "Bearer");
             if (_localUserService.ActiveUser is not null && _localUserService.ActiveUser.Role is not null)
             {
-                Console.WriteLine("hello yes the user has a role");
                 foreach (var scope in _localUserService.ActiveUser.Role.Scopes)
                     identity.AddClaim(new Claim("scope", scope));
             }
